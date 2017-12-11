72b6ba72aba0ed3bb3a65abc4cc61304d608fc41
Paul Rowe <prowe@sourceallies.com>
2017-12-10 20:42:46 -0600

    adding dynamoDB script

---
+++ local-dynamoDB.sh
@@ -0,0 +1,11 @@
+
+if ! dynamoDB/*; then
+    echo "dynamoDB does not exist. downloading"
+    mkdir -p ./dynamoDB
+    curl https://s3-us-west-2.amazonaws.com/dynamodb-local/dynamodb_local_latest.tar.gz | tar xvz -C dynamoDB
+fi
+
+java \
+    -Djava.library.path=./dynamoDB/DynamoDBLocal_lib \
+    -jar dynamoDB/DynamoDBLocal.jar \
+    -inMemory

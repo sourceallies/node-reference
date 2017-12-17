090b09db5787be214325c4ef0f9c8e1356990c42
Paul Rowe <paul.w.rowe@gmail.com>
2017-12-17 10:48:44 -0600

    fixing dynamodb script

--- local-dynamoDB.sh
+++ local-dynamoDB.sh
@@ -1,8 +1,11 @@
+#!/bin/bash
 
-if ! dynamoDB/*; then
+set -e
+
+if [ -z "$(ls -A ./dynamoDB 2>/dev/null)" ]; then
     echo "dynamoDB does not exist. downloading"
     mkdir -p ./dynamoDB
-    curl https://s3-us-west-2.amazonaws.com/dynamodb-local/dynamodb_local_latest.tar.gz | tar xvz -C dynamoDB
+    curl --silent https://s3-us-west-2.amazonaws.com/dynamodb-local/dynamodb_local_latest.tar.gz | tar xvz -C dynamoDB
 fi
 
 java \

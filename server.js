b4178f5b8bf872907074ed8bc7905f620ed2ac2b
Paul Rowe <paul.w.rowe@gmail.com>
2018-01-01 11:20:47 -0600

    working on auth

--- server.js
+++ server.js
@@ -2,12 +2,15 @@ const Koa = require('koa');
 const Router = require('koa-router');
 const bodyParser = require('koa-bodyparser');
 
-process.env.AWS_XRAY_DEBUG_MODE=true;
+process.env.AWS_XRAY_DEBUG_MODE = true;
+process.env.DEBUG = 'jwks';
+
 const trackRequests = require('./common/trackRequests');
 
 const app = new Koa();
 
 app.use(trackRequests('ProductService-localhost'));
+app.use(setupAuthentication());
 app.use(bodyParser());
 app.use(buildRouter().routes());
 
@@ -27,4 +30,19 @@ function buildRouter() {
     router.get('/products/:id', require('./products/getProductById'));
     router.delete('/products/:id', require('./products/deleteProduct'));
     return router;
+}
+
+function setupAuthentication() {
+    const jwt = require('koa-jwt');
+    const jwksRsa = require('jwks-rsa');
+
+    return jwt({
+        secret: jwksRsa.koaJwtSecret({
+            cache: true,
+            rateLimit: true,
+            jwksRequestsPerMinute: 2,
+            jwksUri: 'https://www.googleapis.com/oauth2/v3/certs'
+        }),
+        algorithms: ['RS256']
+    });
 }

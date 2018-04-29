e2357d932a809f18404d38361efeb00ee9a6d7e6
Paul Rowe <prowe@sourceallies.com>
2018-04-28 21:37:22 -0500

    link header attempt

--- products/listProducts.spec.js
+++ products/listProducts.spec.js
@@ -5,13 +5,20 @@ const proxyquire = require('proxyquire');
 describe('products', function () {
 
     describe('listProducts', function () {
-
         beforeEach(function () {
             this.response = {
                 Items: [{},{}]
             }
 
-            this.context = {};
+            this.context = {
+                query: {},
+                response: {
+                    headers: {},
+                    set(field, value) {
+                        this.headers[field] = value;
+                    }
+                }
+            };
 
             this.awsResult = {
                 promise: () => Promise.resolve(this.response)
@@ -38,9 +45,41 @@ describe('products', function () {
             expect(this.documentClient.scan.calls.argsFor(0)[0].Segment).toBe(seg);
         });
 
-        it('should return the product list', async function () {
+        it('should return the product list', async function() {
             await this.listProducts(this.context);
             expect(this.context.body).toEqual(this.response.Items);
+        });
+
+        it('should limit the scan to 25 items', async function() {
+            await this.listProducts(this.context);
+            expect(this.documentClient.scan.calls.argsFor(0)[0].Limit).toEqual(25);
+        });
+
+        
+        describe('pagination', function() {
+            it('should not return a Link header if the returned LastEvaluatedKey is undefined', async function() {
+                delete this.response.LastEvaluatedKey;
+                await this.listProducts(this.context);
+                expect(this.context.response.headers.Link).toBeUndefined();
+            });
+
+            it('should return a properly formatted link header when LastEvaluatedKey is returned', async function() {
+                this.response.LastEvaluatedKey = {
+                    id: 'id123'
+                };
+                await this.listProducts(this.context);
+                expect(this.context.response.headers.link)
+                    .toEqual('</products?_lek=id123>; rel="next"');
+            });
+
+            it('should pass the _lek param to Dyanmo if this is a pagination request', async function() {
+                this.context.query = {
+                    _lek: 'key123'
+                };
+                await this.listProducts(this.context);
+                expect(this.documentClient.scan.calls.argsFor(0)[0].ExclusiveStartKey)
+                    .toEqual({id: 'key123'});
+            });
         });
     });
 });

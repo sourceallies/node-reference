89fe5a2686d78de38e012b0f0a842459b28bdd60
prowe <paul.w.rowe@gmail.com>
2018-05-15 19:09:35 -0500

    extracting hello handler (#5)

---
+++ hello.spec.js
@@ -0,0 +1,19 @@
+
+const proxyquire = require('proxyquire');
+
+describe('hello handler', function() {
+    beforeEach(function() {
+        this.context = {};
+        this.hello = proxyquire('./hello', {});
+    });
+
+    it('should respond with a 200 status', async function() {
+        await this.hello(this.context);
+        expect(this.context.status).toEqual(200);
+    });
+
+    it('should responsd with a hello message', async function() {
+        await this.hello(this.context);
+        expect(this.context.body).toEqual({message: 'hello'});
+    });
+});

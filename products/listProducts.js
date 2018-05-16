bafc74db4ce18927fd76659d9ec0dadc27808012
prowe <paul.w.rowe@gmail.com>
2018-05-15 19:11:03 -0500

    link header attempt (#2)

--- products/listProducts.js
+++ products/listProducts.js
@@ -1,13 +1,37 @@
 'use strict';
 
 const documentClient = require('./documentClient');
+const formatLinkHeader = require('format-link-header');
 const productsTableName = process.env.PRODUCTS_TABLE_NAME || 'Products';
+
+function getExclusiveStartKey(ctx) {
+    if (ctx.query && ctx.query._lek) {
+        return {
+            id: ctx.query._lek
+        };
+    }
+}
+
+function addLinkHeaderIfNeeded(ctx, lastEvaluatedKey) {
+    if (lastEvaluatedKey) {
+        const link = {
+            next: {
+                rel: 'next',
+                url: `/products?_lek=${lastEvaluatedKey.id}`
+            }
+        };
+        ctx.response.set('link', formatLinkHeader(link));
+    }
+}
 
 module.exports = async function getProductList(ctx) {
     const scanOutput = await documentClient.scan({
         Segment: ctx.segment,
-        TableName: productsTableName
+        TableName: productsTableName,
+        Limit: 25,
+        ExclusiveStartKey: getExclusiveStartKey(ctx)
     }).promise();
 
+    addLinkHeaderIfNeeded(ctx, scanOutput.LastEvaluatedKey);
     ctx.body = scanOutput.Items;
 };

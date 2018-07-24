eaf68f98c530be68b64e8b69ce36b800e1891871
Paul Rowe <prowe@sourceallies.com>
2018-07-24 18:38:17 -0500

    adding pagination

--- products/listProducts.js
+++ products/listProducts.js
@@ -1,13 +1,36 @@
 const AWS = require('aws-sdk');
 const documentClient = new AWS.DynamoDB.DocumentClient();
+const formatLinkHeader = require('format-link-header');
 const productsTableName = process.env.PRODUCTS_TABLE_NAME || 'Products';
+
+function getExclusiveStartKey(ctx) {
+    if (ctx.query && ctx.query._lek) {
+        return {
+            id: ctx.query._lek
+        };
+    }
+}
+
+function addLinkHeaderIfNeeded(ctx, lastEvaluatedKey) {
+    if (lastEvaluatedKey) {
+        const link = {
+            next: {
+                rel: 'next',
+                url: `/products?_lek=${lastEvaluatedKey.id}`
+            }
+        };
+        ctx.response.set('link', formatLinkHeader(link));
+    }
+}
 
 module.exports = async function getProductList(ctx) {
     const scanOutput = await documentClient.scan({
         Segment: ctx.segment,
         TableName: productsTableName,
-        Limit: 25
+        Limit: 25,
+        ExclusiveStartKey: getExclusiveStartKey(ctx)
     }).promise();
 
+    addLinkHeaderIfNeeded(ctx, scanOutput.LastEvaluatedKey);
     ctx.body = scanOutput.Items;
 };
--- products/listProducts.spec.js
+++ products/listProducts.spec.js
@@ -50,5 +50,31 @@ describe('products', function () {
             await this.listProducts(this.context);
             expect(this.documentClient.scan.calls.argsFor(0)[0].Limit).toEqual(25);
         });
+
+        describe('pagination', function() {
+            it('should not return a Link header if the returned LastEvaluatedKey is undefined', async function() {
+                delete this.response.LastEvaluatedKey;
+                await this.listProducts(this.context);
+                expect(this.context.response.headers.Link).toBeUndefined();
+            });
+        
+            it('should return a properly formatted link header when LastEvaluatedKey is returned', async function() {
+                this.response.LastEvaluatedKey = {
+                    id: 'id123'
+                };
+                await this.listProducts(this.context);
+                expect(this.context.response.headers.link)
+                    .toEqual('</products?_lek=id123>; rel="next"');
+            });
+        
+            it('should pass the _lek param to Dyanamo if this is a pagination request', async function() {
+                this.context.query = {
+                    _lek: 'key123'
+                };
+                await this.listProducts(this.context);
+                expect(this.documentClient.scan.calls.argsFor(0)[0].ExclusiveStartKey)
+                    .toEqual({id: 'key123'});
+            });
+        });
     });
 });

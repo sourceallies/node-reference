2b9ce14c62299b3868f291a2ceb8159c468d22ac
Paul Rowe <prowe@sourceallies.com>
2018-07-25 19:19:23 -0500

    adding get

---
+++ products/getProductById.js
@@ -0,0 +1,19 @@
+'use strict';
+
+const AWS = require('aws-sdk');
+const documentClient = new AWS.DynamoDB.DocumentClient();
+const productsTableName = process.env.PRODUCTS_TABLE_NAME || 'Products';
+
+module.exports = async function(ctx) {
+    let id = ctx.params.id;
+    const result = await documentClient.get({
+        TableName: productsTableName,
+        Key: {id},
+    }).promise();
+
+    if (!result.Item) {
+        ctx.status = 404;
+    } else {
+        ctx.body = result.Item;
+    }
+};
---
+++ products/getProductById.spec.js
@@ -0,0 +1,61 @@
+'use strict';
+
+const proxyquire = require('proxyquire');
+
+describe('products', function () {
+
+    describe('getProductById', function () {
+
+        beforeEach(function () {
+            this.response = {
+                Item: {}
+            }
+
+            this.context = {
+                params: {}
+            };
+
+            this.awsResult = {
+                promise: () => Promise.resolve(this.response)
+            };
+            const documentClient = this.documentClient ={
+                get: (params) => this.awsResult
+            };
+            spyOn(this.documentClient, 'get').and.callThrough();
+
+            this.getProductById = proxyquire('./getProductById', {
+                'aws-sdk': {
+                    DynamoDB: {
+                        DocumentClient: function() {
+                            return documentClient;
+                        }
+                    }
+                }
+            });
+        });
+
+        it('should pass the correct TableName to documentClient.get', async function () {
+            await this.getProductById(this.context);
+            expect(this.documentClient.get.calls.argsFor(0)[0].TableName).toEqual('Products');
+        });
+
+        it('should pass the id to documentClient.get', async function() {
+            this.context.params.id = 'abc';
+            await this.getProductById(this.context);
+            expect(this.documentClient.get.calls.argsFor(0)[0].Key.id).toEqual('abc');
+        });
+
+        it('should return the product', async function () {
+            await this.getProductById(this.context);
+            expect(this.context.body).toEqual(this.response.Item);
+        });
+
+        it('should return a 404 if the product does not exist', async function () {
+            delete this.response.Item;
+
+            await this.getProductById(this.context);
+
+            expect(this.context.status).toEqual(404);
+        });
+    });
+});
